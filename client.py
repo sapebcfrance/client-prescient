bade5fcb9cab056c4e8e4113566e316f7ee38eb0
AMMOUIAL <william.ammouial@sap.com>
2020-09-08 11:46:57 +0200

    dev

--- client.py
+++ client.py
@@ -13,43 +13,43 @@ import websockets
 
 
 class Pack:
-    def __init__(self):
-        self.content = []
+	def __init__(self):
+		self.content = []
 
-    def add(self, frame):
-        self.content.append(frame)
+	def add(self, frame):
+		self.content.append(frame)
 
-    def generate(self):
-        data = b""
+	def generate(self):
+		data = b""
 
-        for frame in self.content:
-            fby = cv2.imencode(".jpg", frame)[1].tostring()
-            lencompf = struct.pack("l", len(fby))
+		for frame in self.content:
+			fby = cv2.imencode(".jpg", frame)[1].tostring()
+			lencompf = struct.pack("l", len(fby))
 
-            data += lencompf + fby
+			data += lencompf + fby
 
-        data = gzip.compress(data)
-        print("TOTAL : " + str(len(data)))
-        return data
+		data = gzip.compress(data)
+		print("TOTAL : " + str(len(data)))
+		return data
 
-    @staticmethod
-    def retrieve(data):
-        frames = []
-        i = 0
+	@staticmethod
+	def retrieve(data):
+		frames = []
+		i = 0
 
-        data = gzip.decompress(data)
-        lendata = len(data)
+		data = gzip.decompress(data)
+		lendata = len(data)
 
-        while i <= lendata:
-            if len(data[i:i+8]) == 0:
-                break
+		while i <= lendata:
+			if len(data[i:i+8]) == 0:
+				break
 
-            lenframe = struct.unpack("l", data[i:i+8])[0]  # struct L length
-            print(lenframe)
-            frames.append(cv2.imdecode(np.fromstring(data[i+8:i+8+lenframe], np.uint8), cv2.IMREAD_COLOR))
-            i += 8 + lenframe
+			lenframe = struct.unpack("l", data[i:i+8])[0]  # struct L length
+			print(lenframe)
+			frames.append(cv2.imdecode(np.fromstring(data[i+8:i+8+lenframe], np.uint8), cv2.IMREAD_COLOR))
+			i += 8 + lenframe
 
-        return frames
+		return frames
 
 
 # rtsp://admin:sapebc@192.168.3.194:554//h264Preview_01_main
@@ -57,7 +57,7 @@ class Pack:
 rpiName = socket.gethostname()
 
 with open("ips.txt", "r") as readFile:
-    readFileData = readFile.read()
+	readFileData = readFile.read()
 
 logs = open("logs.txt", "a")
 #sys.stdout = logs
@@ -74,18 +74,18 @@ time.sleep(2.0)
 
 
 async def flux():
-    uri = "wss://prescient2.cfapps.eu10.hana.ondemand.com/ws"
-    async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
-        while True:
-        
-            pack = Pack()
-            for vs in videoStreams:
-                pack.add(vs.read())
-        
-            await websocket.send(pack.generate())
-            print("Pack sent !")
+	uri = "wss://prescient2.cfapps.eu10.hana.ondemand.com/ws"
+	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		while True:
 
-            greeting = await websocket.recv()
-            print(f"< {greeting}")
+			pack = Pack()
+			for vs in videoStreams:
+				pack.add(vs.read())
+
+			await websocket.send(pack.generate())
+			print("Pack sent !")
+
+			greeting = await websocket.recv()
+			print(f"< {greeting}")
 
 asyncio.get_event_loop().run_until_complete(flux())
