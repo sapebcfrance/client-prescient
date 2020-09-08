e941902497f17bad16ae310e5c3f5539de4901c1
AMMOUIAL <william.ammouial@sap.com>
2020-09-08 15:42:20 +0200

    dev2

--- client.py
+++ client.py
@@ -88,4 +88,42 @@ async def flux():
 			greeting = await websocket.recv()
 			print(f"< {greeting}")
 
-asyncio.get_event_loop().run_until_complete(flux())
+# asyncio.get_event_loop().run_until_complete(flux())
+
+
+async def cam0():
+	uri = "wss://prescient2.cfapps.eu10.hana.ondemand.com/ws/0"
+	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		while True:
+			frame = videoStreams[0].read()
+			await websocket.send(np.frombuffer(cv2.imencode(".jpg", frame)[1], dtype=np.uint8).tobytes())
+			print("CAM 0 SENT")
+			greeting = await websocket.recv()
+			print(f"< {greeting}")
+
+
+async def cam1():
+	uri = "wss://prescient2.cfapps.eu10.hana.ondemand.com/ws/1"
+	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		while True:
+			frame = videoStreams[0].read()
+			await websocket.send(np.frombuffer(cv2.imencode(".jpg", frame)[1], dtype=np.uint8).tobytes())
+			print("CAM 1 SENT")
+			greeting = await websocket.recv()
+			print(f"< {greeting}")
+
+
+async def cam2():
+	uri = "wss://prescient2.cfapps.eu10.hana.ondemand.com/ws/2"
+	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		while True:
+			frame = videoStreams[0].read()
+			await websocket.send(np.frombuffer(cv2.imencode(".jpg", frame)[1], dtype=np.uint8).tobytes())
+			print("CAM 2 SENT")
+			greeting = await websocket.recv()
+			print(f"< {greeting}")
+
+
+loop = asyncio.get_event_loop()
+cors = asyncio.wait([cam0(), cam1(), cam2()])
+loop.run_until_complete(cors)
