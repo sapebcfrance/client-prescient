8514d6a119f36e2133df7eb66319ac0b09bde229
AMMOUIAL <william.ammouial@sap.com>
2020-09-10 15:01:26 +0200

    end

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from videostream import VideoStream
+
 import sys
 import socket
 import time
@@ -12,6 +13,11 @@ import asyncio
 import websockets
 
 
+# Boitier prescient justement
+HASH = "ae6d3daf613bad5033d5ee4d1fd6b8a999fe61f71d9e8a128571af56e225c396"
+DEBUG = True
+
+
 # rtsp://admin:sapebc@192.168.3.194:554//h264Preview_01_main
 
 rpiName = socket.gethostname()
@@ -19,7 +25,8 @@ rpiName = socket.gethostname()
 with open("ips.txt", "r") as readFile:
 	readFileData = readFile.read()
 
-sys.stdout = open("logs.txt", "a")
+if not DEBUG:
+	sys.stdout = open("logs.txt", "a")
 
 
 cameraIPs = [x for x in readFileData.split("\n") if x != ""]
@@ -32,6 +39,11 @@ time.sleep(2)
 async def cam0():
 	uri = "wss://prescient.cfapps.eu10.hana.ondemand.com/ws/0"
 	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		await websocket.send(HASH.encode("utf-8"))
+		print("CAM 0 connecting")
+		connection = await websocket.recv()
+		print(f"{connection}")
+
 		while True:
 			frame = videoStreams[0].read()
 			await websocket.send(np.frombuffer(cv2.imencode(".jpg", frame)[1], dtype=np.uint8).tobytes())
@@ -43,6 +55,11 @@ async def cam0():
 async def cam1():
 	uri = "wss://prescient.cfapps.eu10.hana.ondemand.com/ws/1"
 	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		await websocket.send(HASH.encode("utf-8"))
+		print("CAM 1 connecting")
+		connection = await websocket.recv()
+		print(f"{connection}")
+
 		while True:
 			frame = videoStreams[1].read()
 			await websocket.send(np.frombuffer(cv2.imencode(".jpg", frame)[1], dtype=np.uint8).tobytes())
@@ -54,6 +71,11 @@ async def cam1():
 async def cam2():
 	uri = "wss://prescient.cfapps.eu10.hana.ondemand.com/ws/2"
 	async with websockets.connect(uri, max_size=2**30, close_timeout=30) as websocket:
+		await websocket.send(HASH.encode("utf-8"))
+		print("CAM 2 connecting")
+		connection = await websocket.recv()
+		print(f"{connection}")
+
 		while True:
 			frame = videoStreams[2].read()
 			await websocket.send(np.frombuffer(cv2.imencode(".jpg", frame)[1], dtype=np.uint8).tobytes())
